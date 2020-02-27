cba3fb815212b4c4fb6588b7bdefc8b26d63b2ee
Detmon0410 <s6101012610061@email.kmutnb.ac.th>
2020-02-27 14:47:49 +0700

    Create calculator_GET app

---
+++ calculator_GET/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Calculate_log(models.Model):
+    name = models.TextField(null=True)
+    db_cal = models.CharField(max_length=211111111111)
---
+++ calculator_GET/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+from django.contrib import admin
+
+urlpatterns = [
+    # link path to views.py
+    path('', views.index, name='index'),
+    path('calc', views.calculate, name='calculate'),
+    path('admin/', admin.site.urls),
+
+]
